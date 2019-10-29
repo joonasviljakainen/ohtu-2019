736e5838134cfe6481af46677d1b8fd26d44011a
Joonas Viljakainen <joonasviljakainen@gmail.com>
2019-10-29 21:45:36 +0200

    added test file for statistics

---
+++ viikko1/tehtavat14-16/NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,66 @@
+
+package ohtuesimerkki;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StatisticsTest {
+
+    Statistics stats;
+
+    Reader readerStub = new Reader() {
+ 
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<>();
+ 
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri",   "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+            players.add(new Player("Lemmings", "ALA", 66, 49));
+ 
+            return players;
+        }
+    };
+
+    @Before
+    public void setUp() {
+        stats = new Statistics(readerStub);
+    }
+
+    @Test
+    public void fetchesPlayersOfTeam() {
+        List<Player> team = stats.team("ALA");
+        assertEquals(1, team.size());
+        team = stats.team("EDM");
+        assertEquals(3, team.size());
+    }
+
+    @Test
+    public void retrievesTopScorers() {
+        List<Player> scorers = stats.topScorers(1);
+        assertEquals(2, scorers.size());
+        assertEquals(scorers.get(0).getName(), "Gretzky");
+        assertEquals(scorers.get(1).getName(), "Lemmings");
+    }
+
+    @Test
+    public void canSearch() {
+        Player p = stats.search("Ku");
+        assertEquals("Kurri", p.getName());
+    }
+
+    @Test
+    public void returnsNullIfPlayerNotFound() {
+        Player p = stats.search("Ibrahimovich");
+        assertNull(p);
+    }
+
+}
