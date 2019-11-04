24859ef1a653834f8fd2414debba1e6d88261a96
Joonas Viljakainen <joonasviljakainen@gmail.com>
2019-11-04 20:42:18 +0200

    halfway through first exercise

---
+++ viikko2/src/main/java/ohtu/Multiplier.java
@@ -0,0 +1,13 @@
+packate ohtu;
+
+public class Multiplier {
+	private int vlaue;
+	public Multiplier(int value) {
+		this.vlaue = value;
+	}
+
+	public int multipliedBy(int other) {
+		return vlaue * other;
+	}
+
+}
