40eed06bca2ecdcae8452fe7bbb15c4735d3bda5
Joonas Viljakainen <joonasviljakainen@gmail.com>
2019-11-22 19:44:43 +0200

    added test with two separate products

--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -34,5 +34,49 @@ public class KauppaTest {
         verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(),anyInt());   
         // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
     }
+
+    @Test
+    public void ostoksenPaatyttyaKivastiKutsutaanOikeillaParametreilla() {
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);              
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     
+        k.tilimaksu("pekka", "12345");
+        
+        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 5);   
+    }
+
+    @Test
+    public void useidenOstostenKanssaKutsutaanOikein() {
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        when(viite.uusi()).thenReturn(66);
+
+        Varasto varasto = mock(Varasto.class);
+        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);              
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     
+
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "pulla", 12));
+        k.lisaaKoriin(1);     
+
+        k.tilimaksu("harri", "54321");
+        
+        verify(pankki).tilisiirto("harri", 66, "54321", "33333-44455", 5 + 12);   
+    }
 }
 
