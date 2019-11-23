f229bcfaf6c1d924028669609ac9c349d362eb14
Joonas Viljakainen <joonasviljakainen@gmail.com>
2019-11-23 11:59:09 +0200

    added spacing for clarity

--- viikko4/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/Tennis/src/main/java/ohtu/TennisGame.java
@@ -21,25 +21,25 @@ public class TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+        int tempScore = 0;
+        if (m_score1 == m_score2)
         {
             switch (m_score1)
             {
                 case 0:
-                        score = "Love-All";
+                    score = "Love-All";
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                    score = "Fifteen-All";
                     break;
                 case 2:
-                        score = "Thirty-All";
+                    score = "Thirty-All";
                     break;
                 case 3:
-                        score = "Forty-All";
+                    score = "Forty-All";
                     break;
                 default:
-                        score = "Deuce";
+                    score = "Deuce";
                     break;
                 
             }
