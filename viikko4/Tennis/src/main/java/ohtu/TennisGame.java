013808aa33fc5523e34dbe4760bcab20834f1045
Joonas Viljakainen <joonasviljakainen@gmail.com>
2019-11-23 12:09:37 +0200

    renamed score parameters; added spacing

--- viikko4/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/Tennis/src/main/java/ohtu/TennisGame.java
@@ -4,8 +4,8 @@ import java.security.InvalidParameterException;
 
 public class TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int player1Score = 0;
+    private int player2Score = 0;
     private String player1Name;
     private String player2Name;
 
@@ -16,55 +16,54 @@ public class TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName == this.player1Name) {
-            m_score1 += 1;
+            player1Score++;
         } else if (playerName == this.player2Name) {
-            m_score2 += 1;
+            player2Score++;
         } else {
             throw new InvalidParameterException("No player by name " + playerName);
+        }
+    }
+
+    public String getEqualScore() {
+        switch (player1Score) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            case 3:
+                return "Forty-All";
+            default:
+                return "Deuce";
         }
     }
 
     public String getScore() {
         String score = "";
         int tempScore = 0;
-        if (m_score1 == m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
+        
+        if (player1Score == player2Score) {
+            return getEqualScore();    
+        } else if (player1Score>=4 || player2Score>=4) {
+
+            int minusResult = player1Score - player2Score;
+            if (minusResult==1) return "Advantage player1";
+            else if (minusResult ==-1) return "Advantage player2";
+            else if (minusResult>=2) return "Win for player1";
+            else return "Win for player2";
+
+        } else {
+
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) {
+                    tempScore = player1Score;
+                }
+                else { 
+                    score += "-"; 
+                    tempScore = player2Score;
+                }
+                switch(tempScore) {
                     case 0:
                         score+="Love";
                         break;
@@ -79,6 +78,7 @@ public class TennisGame {
                         break;
                 }
             }
+
         }
         return score;
     }
