695da349938b9c31bf4b074f3526816b871420e0
Joonas Viljakainen <joonasviljakainen@gmail.com>
2019-12-09 18:45:42 +0200

    added NOT for task 2

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -10,18 +10,20 @@ public class Main {
         // "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
-          
-        Matcher m = new And( new HasAtLeast(5, "goals"),
-                             new HasAtLeast(5, "assists"),
-                             new PlaysIn("PHI")
-        );
-        
+
+        Matcher m = new And(new HasAtLeast(5, "goals"), new HasAtLeast(5, "assists"), new PlaysIn("PHI"));
+
         for (Player player : stats.matches(m)) {
             System.out.println(player);
         }
 
         Matcher b = new All();
-        for (Player p: stats.matches(b)) {
+        for (Player p : stats.matches(b)) {
+            System.out.println(p);
+        }
+
+        Matcher c = new Not(new HasAtLeast(3, "goals"), new HasAtLeast(3, "assists"), new PlaysIn("CGY"));
+        for (Player p : stats.matches(c)) {
             System.out.println(p);
         }
     }
