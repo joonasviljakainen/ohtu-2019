dd45803db65b3e34d6fd871aa1171363b1431c36
Joonas Viljakainen <joonasviljakainen@gmail.com>
2019-12-09 18:59:05 +0200

    added fewerThan and tests for task2; finished task2

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -1,5 +1,7 @@
 package statistics;
 
+import java.util.List;
+
 import statistics.matcher.*;
 
 public class Main {
@@ -12,19 +14,32 @@ public class Main {
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
 
         Matcher m = new And(new HasAtLeast(5, "goals"), new HasAtLeast(5, "assists"), new PlaysIn("PHI"));
-
-        for (Player player : stats.matches(m)) {
-            System.out.println(player);
-        }
+        printPlayers(stats.matches(m));
 
         Matcher b = new All();
-        for (Player p : stats.matches(b)) {
+        printPlayers(stats.matches(b));
+
+        Matcher c = new Not(new HasAtLeast(3, "goals"), new HasAtLeast(3, "assists"), new PlaysIn("CGY"));
+        printPlayers(stats.matches(c));
+
+        Matcher fewerTest = new And(new HasFewerThan(4, "goals"), new HasFewerThan(3, "assists"), new PlaysIn("CGY"));
+        printPlayers(stats.matches(fewerTest));
+
+        // TESTI: pitäisi olla samat
+
+        Matcher m2 = new And(new Not(new HasAtLeast(1, "goals")), new PlaysIn("NYR"));
+        printPlayers(stats.matches(m2));
+
+        Matcher m3 = new And(new HasFewerThan(1, "goals"), new PlaysIn("NYR"));
+        printPlayers(stats.matches(m3));
+
+    }
+
+    public static void printPlayers(List<Player> matches) {
+        for (Player p : matches) {
             System.out.println(p);
         }
 
-        Matcher c = new Not(new HasAtLeast(3, "goals"), new HasAtLeast(3, "assists"), new PlaysIn("CGY"));
-        for (Player p : stats.matches(c)) {
-            System.out.println(p);
-        }
+        System.out.println();
     }
 }
